d5b6f3130fd7341fe52c9a2f9a9d05738edb5413
Erich Schubert <erich.schubert@tu-dortmund.de>
2022-10-15 16:36:56 +0200

    Work around OOM issues with arm64 docker container

--- .github/workflows/build-wheels.sh
+++ .github/workflows/build-wheels.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e -x
 
+# update rust
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
 rustup default stable
+
+# Workaround for OOM issues, https://github.com/rust-lang/cargo/issues/10583
+echo "[net]" >> "$HOME/.cargo/config.toml"
+echo "git-fetch-with-cli = true" >> "$HOME/.cargo/config.toml"
 
 # build wheels
 for PYBIN in /opt/python/cp3[891]*/bin; do
